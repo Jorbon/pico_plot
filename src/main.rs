d89f51e77202cfe5bc0399da56c8d76ae4466d4f
Jorbon <ben.a.phillips@outlook.com>
2023-10-23 23:12:01 -0500

    flipped a default

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use serialport::SerialPort;
+use serialport::{SerialPort, SerialPortInfo};
 use speedy2d::{window::{WindowCreationOptions, WindowSize, WindowPosition, WindowHandler, WindowHelper, MouseScrollDistance, VirtualKeyCode, KeyScancode, MouseButton}, dimen::{Vector2, Vec2, UVec2}, Window, Graphics2D, image::{ImageDataType, ImageSmoothingMode}};
 use plotters::prelude::*;
 
@@ -10,7 +10,6 @@ fn main() {
 	// List available serial ports
 	
 	let ports = serialport::available_ports().unwrap_or_else(|err| panic!("Could not get info on serial ports: {err}"));
-	let n = ports.len();
 	
 	for p in ports.clone() {
 		println!("Name: {}", p.port_name);
@@ -35,98 +34,7 @@ fn main() {
 	let options = WindowCreationOptions::new_windowed(WindowSize::ScaledPixels(size), Some(WindowPosition::Center)).with_vsync(true).with_always_on_top(cfg.always_on_top);
 	let window = Window::new_with_options("Picoammeter Readings", options).unwrap_or_else(|err| panic!("Could not create a plot window: {err}"));
 	
-	let port = if n == 0 {
-		println!("No serial port connections found.");
-		None
-		
-	} else {
-		
-		let mut port_name = None;
-		
-		// Search for matching serial number
-		for p in &ports {
-			if let serialport::SerialPortType::UsbPort(info) = &p.port_type {
-				if let Some(sn) = &info.serial_number {
-					if *sn == cfg.serial_number {
-						port_name = Some(&p.port_name);
-						println!("Using port {} with matching serial number '{}'", p.port_name, *sn);
-						break;
-					}
-				}
-			}
-		}
-		
-		// Next search for matching manufacturer
-		if let None = port_name {
-			for p in &ports {
-				if let serialport::SerialPortType::UsbPort(info) = &p.port_type {
-					if let Some(m) = &info.manufacturer {
-						if *m == cfg.manufacturer {
-							port_name = Some(&p.port_name);
-							println!("No serial number match, using port {} with matching manufacturer '{}'", p.port_name, *m);
-							break;
-						}
-					}
-				}
-			}
-		}
-		
-		// Next use the first usb connection
-		if let None = port_name {
-			for p in &ports {
-				if let serialport::SerialPortType::UsbPort(_info) = &p.port_type {
-					port_name = Some(&p.port_name);
-					println!("No serial number or manufacturer match, using port {}", p.port_name);
-					break;
-				}
-			}
-		}
-		
-		let port_attempt = serialport::new(port_name.unwrap_or(&ports[0].port_name), 9600)
-		.stop_bits(serialport::StopBits::One)
-		.data_bits(serialport::DataBits::Eight)
-		.open();
-		
-		// Open port, just try the first port if none were selected
-		match port_attempt {
-			Ok(mut port) => {
-				let commands = [
-					"*RST",
-					"ARM:COUN 1",
-					"DISP:DIG 5",
-					"SYST:ZCH OFF",
-					"SENS:CURR:NPLC 6",
-					"FORM:ELEM READ",
-					"TRIG:COUN 1"
-				];
-				
-				let mut port_setup = true;
-				
-				for command in commands {
-					let mut c = String::from(command);
-					c.push('\r');
-					match port.write(c.as_bytes()) {
-						Ok(_) => (),
-						Err(_err) => {
-							println!("Couldn't send command: {command}");
-							port_setup = false;
-							break;
-						}
-					};
-					std::thread::sleep(std::time::Duration::from_millis(50));
-				}
-				
-				match port_setup {
-					true => Some(port),
-					false => None
-				}
-			}
-			Err(_) => {
-				println!("Could not open selected port {}", port_name.unwrap_or(&ports[0].port_name));
-				None
-			}
-		}
-	};
+	let port = find_port(&cfg, ports);
 	
 	let w = MyWindowHandler::new(size, &cfg, port);
 	
@@ -177,6 +85,100 @@ pub fn get_config() -> Config {
 		}
 	}
 	
+}
+
+
+pub fn find_port(cfg: &Config, ports: Vec<SerialPortInfo>) -> Option<Box<dyn SerialPort>> {
+	if ports.len() == 0 {
+		println!("No serial port connections found.");
+		return None;
+	}
+	
+	let mut port_name = None;
+	
+	// Search for matching serial number
+	for p in &ports {
+		if let serialport::SerialPortType::UsbPort(info) = &p.port_type {
+			if let Some(sn) = &info.serial_number {
+				if *sn == cfg.serial_number {
+					port_name = Some(&p.port_name);
+					println!("Using port {} with matching serial number '{}'", p.port_name, *sn);
+					break;
+				}
+			}
+		}
+	}
+	
+	// Next search for matching manufacturer
+	if let None = port_name {
+		for p in &ports {
+			if let serialport::SerialPortType::UsbPort(info) = &p.port_type {
+				if let Some(m) = &info.manufacturer {
+					if *m == cfg.manufacturer {
+						port_name = Some(&p.port_name);
+						println!("No serial number match, using port {} with matching manufacturer '{}'", p.port_name, *m);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	// Next use the first usb connection
+	if let None = port_name {
+		for p in &ports {
+			if let serialport::SerialPortType::UsbPort(_info) = &p.port_type {
+				port_name = Some(&p.port_name);
+				println!("No serial number or manufacturer match, using port {}", p.port_name);
+				break;
+			}
+		}
+	}
+	
+	let port_attempt = serialport::new(port_name.unwrap_or(&ports[0].port_name), 9600)
+	.stop_bits(serialport::StopBits::One)
+	.data_bits(serialport::DataBits::Eight)
+	.open();
+	
+	// Open port, just try the first port if none were selected
+	match port_attempt {
+		Ok(mut port) => {
+			let commands = [
+				"*RST",
+				"ARM:COUN 1",
+				"DISP:DIG 5",
+				"SYST:ZCH OFF",
+				"SENS:CURR:NPLC 6",
+				"FORM:ELEM READ",
+				"TRIG:COUN 1"
+			];
+			
+			let mut port_setup = true;
+			
+			for command in commands {
+				let mut c = String::from(command);
+				c.push('\r');
+				match port.write(c.as_bytes()) {
+					Ok(_) => (),
+					Err(_err) => {
+						println!("Couldn't send command: {command}");
+						port_setup = false;
+						break;
+					}
+				};
+				std::thread::sleep(std::time::Duration::from_millis(50));
+			}
+			
+			match port_setup {
+				true => Some(port),
+				false => None
+			}
+		}
+		Err(_) => {
+			println!("Could not open selected port {}", port_name.unwrap_or(&ports[0].port_name));
+			None
+		}
+	}
 }
 
 
@@ -243,7 +245,7 @@ impl MyWindowHandler {
 			buffer: VecDeque::new(),
 			program_start: std::time::Instant::now(),
 			program_start_time_seconds: std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap_or_default().as_secs_f64() % 86400.0 + 3600.0 * cfg.time_zone_hour_offset,
-			minimal: true,
+			minimal: false,
 			follow: true,
 			bg_color: RGBColor(0, 0, 0),
 			fg_color: RGBColor(255, 255, 255),
